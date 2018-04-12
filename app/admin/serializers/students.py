bdefa151fd6fe5ac292eb2c046ee86f1af3f14ad
Arthur Verdier <a.verdier@outlook.fr>
2018-04-12 14:36:07 +0200

    add project id in nested group

--- app/admin/serializers/students.py
+++ app/admin/serializers/students.py
@@ -6,6 +6,11 @@ from .facilitators import facilitator_post_model, facilitator_patch_model
 from .. import api
 
 
+group_nested_with_project = api.inherit('Group nested with project ID', group_nested, {
+    'project_id': fields.String(required=True, description='Project ID', attribute=lambda gr: gr.project.id)
+})
+
+
 student_post_model = api.inherit('Student POST model', facilitator_post_model, {
     'campus': fields.String(required=True, description='Campus ID'),
     'section': fields.String(required=True, description='Section ID')
@@ -23,7 +28,7 @@ student_minimal_model = api.model('Student minimal model', {
 student_model = api.inherit('Student model', student_minimal_model, {
     'email': fields.String(required=True, description='Email'),
     'scopes': fields.List(fields.String(), required=True, description='Scopes'),
-    'groups': fields.List(fields.Nested(group_nested), required=True, description='Groups list')
+    'groups': fields.List(fields.Nested(group_nested_with_project), required=True, description='Groups list')
 })
 
 student_container = api.model('Student container', {
